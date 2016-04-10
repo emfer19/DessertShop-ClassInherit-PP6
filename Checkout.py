d52deb8fb7ce45ab6e81bcc607936d0c10790bbd
Emily Ferretti <emfer19@users.noreply.github.com>
2016-04-10 16:22:53 -0500

    start build, imports

--- Checkout.py
+++ Checkout.py
@@ -8,3 +8,10 @@
 
 '''Honor Code Statement: In keeping with the honor code policies of St.Louis University, the Department of Mathematics and Computer Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort.'''
 
+
+
+from Candy import Candy
+from Cookie import Cookie
+
+
+
