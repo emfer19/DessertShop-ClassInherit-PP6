c22b4f9ab43b16b0828a1a6fc62a5434d177f04b
Emily Ferretti <emfer19@users.noreply.github.com>
2016-04-10 15:40:44 -0500

    class build and unit testing

--- Cookie.py
+++ Cookie.py
@@ -8,3 +8,68 @@
 
 '''Honor Code Statement: In keeping with the honor code policies of St.Louis University, the Department of Mathematics and Computer Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort.'''
 
+#derived class from DessertItem class
+
+from DessertItem import DessertItem
+class Cookie(DessertItem):
+  """derived DessertItem class:
+  specialized for Cookie dessert items.
+
+  Calculates cost based on given quantity, and price per dozen
+  
+  Please give PRICE in DOLLARS and CENTS, not just in cents:
+      CORRECT: 1.89
+      INCORRECT: 189"""
+
+  def __init__(self,n='Cookie',q=0,price=0.0):
+    """initialize cookie object with name, quantity, and price per dozen"""
+    DessertItem.__init__(self,n)
+    self.quantity=q
+    self.priceDozen=price
+
+  def getQuantity(self):
+    """returns the quantity of cookies"""
+    return self.quantity
+
+  def getPriceDozen(self):
+    """returns the price per dozen cookies"""
+    return self.priceDozen
+
+  def setQuantity(self,qt):
+    """assigns formal parameter to cookie's quantity"""
+    self.quantity=int(qt)
+
+  def setPriceDozen(self,price):
+    """assigns the formal parameter to cookie's price"""
+    self.priceDozen=float(price)
+
+  def calculateCost(self):
+    """calculates cost of cookie item based off given quantity and price"""
+    cost=(round(self.getQuantity()*(self.getPriceDozen()/12)))
+    self.setCost(cost)
+    return
+
+  def __str__(self):
+    """returns a string containing the cookie's info"""
+    display=DessertItem.__str__(self)
+    display+='\nQuantity: '+str(self.quantity)
+    display+='\nPrice per Dozen: '+str(self.priceDozen)
+    return display
+
+#end of class build
+
+#unit testing
+
+if __name__=='__main__':
+  des1=Cookie('Oatmeal Cookies',4,3.99)
+  des1.calculateCost()
+  print des1.getName()
+  print des1.getCost()
+  print des1.getQuantity()
+  print des1.getPriceDozen()
+  print des1
+
+  des1.setQuantity(1)
+  des1.setPriceDozen(10)
+  des1.calculateCost()
+  print des1
