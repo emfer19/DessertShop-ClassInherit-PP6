e77aad87aa91635ec6b5b0a9871f5e903387768b
Emily Ferretti <ferrettiek@slu.edu>
2016-04-13 10:38:27 -0500

    test file created

---
+++ pp6.py
@@ -0,0 +1,13 @@
+#Name: Emily Ferretti
+#Email: ferrettiek@slu.edu
+#Current date: 4/13/16
+#Course Information: CSCI-1300-01
+#Instructor: Judy Etchison
+
+#Sources Consulted: textbook, my past programming knowledge
+
+'''Honor Code Statement: In keeping with the honor code policies of St.Louis University, the Department of Mathematics and Computer Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort.'''
+
+from Checkout import Checkout
+
+
